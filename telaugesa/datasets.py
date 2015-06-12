ecbdf441b008a6827e2c89035b6f4546335b7b2f
Yuhuang Hu <duguyue100@gmail.com>
2015-06-12 18:01:30 +0800

    new CIFAR-10 build

--- telaugesa/datasets.py
+++ telaugesa/datasets.py
@@ -3,8 +3,10 @@
 This module implements the interface that loads datasets
 
 + MNIST
++ CIFAR-10
 """
 
+import os;
 import gzip;
 import cPickle as pickle;
 
@@ -68,3 +70,54 @@ def load_mnist(dataset):
     rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
             (test_set_x, test_set_y)];
     return rval;
+
+def load_CIFAR_batch(filename):
+    """
+    load single batch of cifar-10 dataset
+    
+    code is adapted from CS231n assignment kit
+    
+    @param filename: string of file name in cifar
+    @return: X, Y: data and labels of images in the cifar batch
+    """
+    
+    with open(filename, 'r') as f:
+        datadict=pickle.load(f);
+        
+        X=datadict['data'];
+        Y=datadict['labels'];
+        
+        X=X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float");
+        Y=np.array(Y);
+        
+        return X, Y;
+        
+        
+def load_CIFAR10(ROOT):
+    """
+    load entire CIFAR-10 dataset
+    
+    code is adapted from CS231n assignment kit
+    
+    @param ROOT: string of data folder
+    @return: Xtr, Ytr: training data and labels
+    @return: Xte, Yte: testing data and labels
+    """
+    
+    xs=[];
+    ys=[];
+    
+    for b in range(1,6):
+        f=os.path.join(ROOT, "data_batch_%d" % (b, ));
+        X, Y=load_CIFAR_batch(f);
+        xs.append(X);
+        ys.append(Y);
+        
+    Xtr=np.concatenate(xs);
+    Ytr=np.concatenate(ys);
+    
+    del X, Y;
+    
+    Xte, Yte=load_CIFAR_batch(os.path.join(ROOT, "test_batch"));
+    
+    return Xtr, Ytr, Xte, Yte;
